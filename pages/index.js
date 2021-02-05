7823e2a867cc39be33770cbd61d779045b4e89a4
youkoucoding <youkousai@gmail.com>
2021-02-05 22:49:20 +0900

    add some style

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
 import Head from 'next/head';
 
 export default function Home({ weather }) {
-
   return (
-    <div className="py-7">
+    <div>
       <Head>
         <title>WeatherApp</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet"></link>
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap'
+          rel='stylesheet'
+        ></link>
+        <meta charSet='utf-8' />
+        <meta
+          name='viewpoint'
+          content='"width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
       </Head>
 
-      <div>
-        <p className="font-serif text-lg  font-maru text-green-300">this is a joke!</p>
-        <p className="container mx-auto shadow-lg  bg-red-300 font-maru text-green-600 text-lg">
+      <div className='container justify-items-center'>
+        <header className='justify-items-center'>what's this</header>
+        <p className='container py-3 mx-3 bg-footer-texture shadow-lg text-gray-800 max-w-5xl font-maru text-lg'>
           伊豆諸島では強風や高波に
           {weather.description.text}
-          <div className='flex justify-items-center font-maru text-gray-700 text-center'>{weather.location.city}</div>
+          <div className='flex justify-items-center font-maru text-gray-700 text-center'>
+            {weather.location.city}
+            {weather.description.publicTime}
+            <img src={weather.copyright.image.url} alt='' />
+          </div>
         </p>
       </div>
-    </div >
+    </div>
   );
 }
 
-
 export async function getStaticProps() {
   try {
-    const res = await fetch('https://weather.tsukumijima.net/api/forecast?city=130011');
+    const res = await fetch(
+      'https://weather.tsukumijima.net/api/forecast?city=130011'
+    );
     const weather = await res.json();
     return {
       props: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'maru': ['Yusei Magic'],
+      maru: ['Yusei Magic'],
     },
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'hero-pattern':
+          "url('https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=840&q=80')",
+        'footer-texture':
+          "url('https://images.unsplash.com/photo-1531386450450-969f935bd522?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80')",
+      }),
+    },
   },
   variants: {
     extend: {},
